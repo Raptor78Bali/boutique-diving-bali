b1a9a6fbe67493a19c7db6e7c23a5e7746361175
Raptor78Bali <dominicbali78@gmail.com>
2026-03-25 11:06:51 +0800

    Replace logo with clean cropped version, simplify header code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,8 +43,8 @@ export default function Header() {
  <Image
  src="/images/logo-icon-only.png"
  alt="Boutique Diving Bali"
- width={200}
- height={200}
+ width={400}
+ height={400}
  className="w-full h-full object-cover"
  />
 </div>
